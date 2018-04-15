23d4d02ad866dd5742942a2857a2fcc3356b9a47
HusKka <33933044+HusKka@users.noreply.github.com>
2018-04-16 01:25:36 +0800

    重做登陆，大厅界面

--- Unity/Hotfix/UI/UILobby/Component/UILobbyComponent.cs
+++ Unity/Hotfix/UI/UILobby/Component/UILobbyComponent.cs
@@ -16,78 +16,70 @@ namespace ETHotfix
 	
 	public class UILobbyComponent : Component
 	{
-		private GameObject enterMap;
-		private Text text;
+		private Text nameText;
+        private Text goldText;
+        private Button addGoldBtn;
+        private Button shopBtn;
+        private Button taskBtn;
+        private Button activityBtn;
+        private Button settingBtn;
+        private GameObject noticeRoot;
+        private Text noticeText;
+        private Button goldPlaceBtn;
+        private Button hundredPlaceBtn;
+        private Button personalPlaceBtn;
 
-		public void Awake()
+        public void Awake()
 		{
 			ReferenceCollector rc = this.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
-			GameObject sendBtn = rc.Get<GameObject>("Send");
-			GameObject sendRpcBtn = rc.Get<GameObject>("SendRpc");
-			sendBtn.GetComponent<Button>().onClick.Add(this.OnSend);
-			sendRpcBtn.GetComponent<Button>().onClick.Add(this.OnSendRpc);
+			nameText = rc.Get<GameObject>("NameText").GetComponent<Text>();
+            goldText = rc.Get<GameObject>("GoldNumText").GetComponent<Text>();
+            addGoldBtn = rc.Get<GameObject>("AddGoldBtn").GetComponent<Button>();
+            shopBtn = rc.Get<GameObject>("ShopBtn").GetComponent<Button>();
+            taskBtn = rc.Get<GameObject>("TaskBtn").GetComponent<Button>();
+            activityBtn = rc.Get<GameObject>("ActivityBtn").GetComponent<Button>();
+            settingBtn = rc.Get<GameObject>("SettingBtn").GetComponent<Button>();
+            noticeRoot = rc.Get<GameObject>("NoticeRoot");
+            noticeText = rc.Get<GameObject>("NoticeText").GetComponent<Text>();
+            goldPlaceBtn = rc.Get<GameObject>("GoldPlaceBtn").GetComponent<Button>();
+            hundredPlaceBtn = rc.Get<GameObject>("HundredPlaceBtn").GetComponent<Button>();
+            personalPlaceBtn = rc.Get<GameObject>("PersonalPlaceBtn").GetComponent<Button>();
 
-			GameObject transfer1Btn = rc.Get<GameObject>("Transfer1");
-			GameObject transfer2Btn = rc.Get<GameObject>("Transfer2");
-			transfer1Btn.GetComponent<Button>().onClick.Add(this.OnTransfer1);
-			transfer2Btn.GetComponent<Button>().onClick.Add(this.OnTransfer2);
-			
-			enterMap = rc.Get<GameObject>("EnterMap");
-			enterMap.GetComponent<Button>().onClick.Add(this.EnterMap);
+            addGoldBtn.onClick.Add(() => OnEmptyBtnClick(addGoldBtn.gameObject));
 
-			this.text = rc.Get<GameObject>("Text").GetComponent<Text>();
-		}
+            shopBtn.onClick.Add(() => OnEmptyBtnClick(shopBtn.gameObject));
+            taskBtn.onClick.Add(() => OnEmptyBtnClick(taskBtn.gameObject));
+            activityBtn.onClick.Add(() => OnEmptyBtnClick(activityBtn.gameObject));
+            settingBtn.onClick.Add(() => OnEmptyBtnClick(settingBtn.gameObject));
 
-		private void OnSend()
-		{
-			// 发送一个actor消息
-			SessionComponent.Instance.Session.Send(new Actor_Test() { Info = "message client->gate->map->gate->client" });
-		}
+            goldPlaceBtn.onClick.Add(OnGoldPlaceBtnClick);
+            hundredPlaceBtn.onClick.Add(() => OnEmptyBtnClick(hundredPlaceBtn.gameObject));
+            personalPlaceBtn.onClick.Add(() => OnEmptyBtnClick(personalPlaceBtn.gameObject));
 
-		private async void OnSendRpc()
-		{
-			try
-			{
-				// 向actor发起一次rpc调用
-				Actor_TestResponse response = (Actor_TestResponse) await SessionComponent.Instance.Session.Call(new Actor_TestRequest() { request = "request actor test rpc" });
-				Log.Info($"recv response: {JsonHelper.ToJson(response)}");
-			}
-			catch (Exception e)
-			{
-				Log.Error(e);
-			}
-		}
+            ApplyUserInfo();
+        }
 
-		private async void OnTransfer1()
-		{
-			try
-			{
-				Actor_TransferResponse response = (Actor_TransferResponse) await SessionComponent.Instance.Session.Call(new Actor_TransferRequest() {MapIndex = 0});
-				Log.Info($"传送成功! {JsonHelper.ToJson(response)}");
-			}
-			catch (Exception e)
-			{
-				Log.Error(e);
-			}
-		}
+        public async void ApplyUserInfo()
+        {
+            //获取玩家数据
+            long userId = ClientComponent.Instance.LocalPlayer.UserID;
+            C2G_GetUserInfo c2G_GetUserInfo_Req = new C2G_GetUserInfo() { UserID = userId };
+            G2C_GetUserInfo g2C_GetUserInfo_Ack = await SessionWrapComponent.Instance.Session.Call(c2G_GetUserInfo_Req) as G2C_GetUserInfo;
 
-		private async void OnTransfer2()
-		{
-			Actor_TransferResponse response = (Actor_TransferResponse)await SessionComponent.Instance.Session.Call(new Actor_TransferRequest() { MapIndex = 1 });
-			Log.Info($"传送成功! {JsonHelper.ToJson(response)}");
-		}
+            //显示用户信息
+            rc.Get<GameObject>("NickName").GetComponent<Text>().text = g2C_GetUserInfo_Ack.NickName;
+            rc.Get<GameObject>("Money").GetComponent<Text>().text = g2C_GetUserInfo_Ack.Money.ToString();
+        }
 
-		private async void EnterMap()
-		{
-			try
-			{
-				G2C_EnterMap g2CEnterMap = (G2C_EnterMap)await SessionComponent.Instance.Session.Call(new C2G_EnterMap());
-				Game.Scene.GetComponent<UIComponent>().Remove(UIType.UILobby);
-			}
-			catch (Exception e)
-			{
-				Log.Error(e);
-			}	
-		}
-	}
+        public void OnEmptyBtnClick(GameObject go)
+        {
+            Log.Info($"OnEmptyBtnClick:{go.name}");
+        }
+
+        public void OnGoldPlaceBtnClick()
+        {
+        }
+
+
+    }
 }
